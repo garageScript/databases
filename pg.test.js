cd47dd7befa0cac860180cf9ecaf711c5ddcd528
pvalderama <34151463+pvalderama@users.noreply.github.com>
2020-03-26 15:35:50 -0700

    pg.test

---
+++ pg.test.js
@@ -0,0 +1,8 @@
+const { createPgAccount } = require('./pg')
+const newClient = require('pg');
+
+describe('testing postgres', () => {
+    it('testing create pg account', () => {
+        expect(1).toEqual()
+    })
+})
