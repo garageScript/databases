f96e5e461608f10e3f4c25ff9e569e74c646ad1d
unknown <widesun91@gmail.com>
2020-04-08 23:14:42 +0900

    add filename in logs

--- log.js
+++ log.js
@@ -4,11 +4,20 @@ const logger = winston.createLogger({
     transports : [
         new winston.transports.Console()
     ]
-})
+})    
 
-logger.info('What rolls down stairs');
-logger.info('alone or in pairs,');
-logger.info('and over your neighbors dog?');
-logger.warn('Whats great for a snack,');
-logger.info('And fits on your back?');
-logger.error('Its log, log, log');
+const output = (file) => {
+    const obj = {}
+    obj.log = (str) => {
+        logger.info(`${file}: ${str}`)
+    }
+    obj.warn = (str) => {
+        logger.warn(`${file}: ${str}`)
+    }
+    obj.error = (str) => {
+        logger.error(`${file}: ${str}`)
+    }
+    return obj
+}
+
+module.exports = output
