bc895e692667b96a080f5288891d2c193f3b25d0
pvalderama <34151463+pvalderama@users.noreply.github.com>
2020-04-27 15:00:43 -0700

    mailer test promise

--- services/mailer.js
+++ services/mailer.js
@@ -1,10 +1,10 @@
 const mailgun = require('mailgun-js');
-const logger = require('../lib/log')
+const logger = require('../lib/log')(__filename)
 require('dotenv').config()
 
-const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.DOMAIN});
+const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
 
-const sendConfirmationEmail = () => {
+const sendConfirmationEmail = () => {    
     const data = {
         from: process.env.SENDER_EMAIL,
         to: process.env.RECEIVER_EMAIL,
@@ -12,12 +12,13 @@ const sendConfirmationEmail = () => {
         text: 'Welcome to C0D3',
         html: "<h1>Confirm your E-mail</h1><button>Confirm</button>"
     };
-    mg.messages().send(data, function(error, body){
-        if(error){
-            logger.error('email failed to send')
-        }
-        logger.info('email successfully sent')
+    
+    mg.messages().send(data).then((returnedData) => {
+        logger.info('results')
+    }).catch((error) => {
+        logger.error('error')
     })
 }
+sendConfirmationEmail()
 
 module.exports = sendConfirmationEmail
--- services/mailer.test.js
+++ services/mailer.test.js
@@ -1,7 +1,8 @@
-const email = require('./mailer');
+jest.mock('../lib/log')
 const mailgun = require('mailgun-js');
-const mockSend = jest.fn(()=>Promise.resolve());
+const email = require('./mailer');
 
+const mockSend = jest.fn()
 jest.mock('mailgun-js', () => {
     return jest.fn(() =>({
         messages: ()=>({
@@ -10,10 +11,27 @@ jest.mock('mailgun-js', () => {
     }))
 })
 
+// const data = {
+//     from: 'mock@mail.com',
+//     to: 'mock@mailer.com',
+//     subject: 'Congratulations!',
+//     text: 'Welcome to C0D3',
+//     html: "<h1>Confirm your E-mail</h1><button>Confirm</button>"
+// };
+
+const logGen = require('../lib/log')
+const logger = {
+  error: jest.fn(),
+  log: jest.fn()
+}
+
+logGen.mockReturnValue(logger)
+
 describe('Test mailgun', ()=>{
-    it('mailgun', ()=>{
+    it('should test if mocksend and mailgun is called', ()=>{
         email()
         expect(mockSend).toHaveBeenCalledTimes(1)
         expect(mailgun).toHaveBeenCalledTimes(1)
+        expect(logger.info).toHaveBeenCalledTimes(1)
     })
 })
