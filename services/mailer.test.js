1a686f9cd3fdf9c3eeac62e847dcd303f2dbd27d
pvalderama <34151463+pvalderama@users.noreply.github.com>
2020-04-30 14:47:20 -0700

    send reject / resolve

--- services/mailer.test.js
+++ services/mailer.test.js
@@ -4,7 +4,9 @@ const mailgun = require('mailgun-js');
 
 //make sendFn reject
 const messages = {
-    sendFn: jest.fn().mockReturnValue( Promise.reject('err'))
+    // sendReject: jest.fn().mockReturnValue( Promise.reject('err')),
+    send: jest.fn().mockReturnValue( Promise.resolve('hello')),
+    sendReject: jest.fn().mockReturnValue( Promise.reject('rejected'))
 }
 
 mailgun.mockImplementation(() => {
@@ -15,28 +17,19 @@ mailgun.mockImplementation(() => {
     }
 })
 
-const sendFn = jest.fn().mockReturnValue( Promise.resolve('hello'))
+const email = require('./mailer');
 
-mailgun.mockImplementation(() => {
-    return{
-        messages: () => {
-            return {
-                send: sendFn
-            }
-        }
-    }
-})
 
 //requiring mailer uses the mailgun function so you want to initialize it after you define it at lines 8-16
 
-const logGen = require('../lib/log')
 const logger = {
-  error: jest.fn(),
-  info: jest.fn()
+    info: jest.fn(),
+    error: jest.fn()
 }
 
+const logGen = require('../lib/log')
 logGen.mockReturnValue(logger)
-const email = require('./mailer');
+
 
 describe('Test mailgun', ()=>{
     beforeEach(() => {
@@ -45,11 +38,20 @@ describe('Test mailgun', ()=>{
     
     it('should test if mocksend and mailgun is called', async ()=>{
         await email.sendConfirmationEmail('paul@github.com', 'token123')
-        expect(sendFn).toHaveBeenCalledTimes(1)
-        expect(sendFn.mock.calls[0][0]).toMatchSnapshot()
-        expect(logger.info.mock.calls[0][0]).toEqual('Confirmation Email successfully sent')        
-        expect(logger.error.mock.calls[0][0]).toEqual("Confirmation Email Error:")
-        // expect(logger.info).toHaveBeenCalledTimes(1)
+        expect(messages.send).toHaveBeenCalledTimes(1)
+        expect(messages.send.mock.calls[0][0]).toMatchSnapshot()        
+        expect(logger.info).toHaveBeenCalledTimes(1)
+        // expect(logger.info.mock.calls[0][0]).toEqual('Confirmation Email successfully sent')        
         
+    })
+    it('should test throw', async ()=>{        
+        try{
+            await messages.sendReject.mockReturnValue(Promise.reject())
+        //     expect(messages.sendReject).toHaveBeenCalledTimes(1)
+        // expec(messages.sendReject.mock.calls[0][0]).toMatchSnapshot()
+        }catch(error){
+            // expect(logger.error).toHaveBeenCalledTimes(1)
+            expect(logger.error.mock.calls[0][0]).toEqual("Confirmation Email Error:")            
+        }
     })
 })
