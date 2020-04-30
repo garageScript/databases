cfa23b58a3fffbf1a9e7819f5664b42b398f2602
pvalderama <34151463+pvalderama@users.noreply.github.com>
2020-04-29 20:44:56 -0700

    logger error test

--- services/mailer.test.js
+++ services/mailer.test.js
@@ -2,6 +2,18 @@ jest.mock('../lib/log')
 jest.mock('mailgun-js')
 const mailgun = require('mailgun-js');
 
+//make sendFn reject
+const messages = {
+    sendFn: jest.fn().mockReturnValue( Promise.reject('err'))
+}
+
+mailgun.mockImplementation(() => {
+    return{
+        messages: () => {
+            return messages
+        }
+    }
+})
 
 const sendFn = jest.fn().mockReturnValue( Promise.resolve('hello'))
 
@@ -17,7 +29,6 @@ mailgun.mockImplementation(() => {
 
 //requiring mailer uses the mailgun function so you want to initialize it after you define it at lines 8-16
 
-
 const logGen = require('../lib/log')
 const logger = {
   error: jest.fn(),
@@ -25,7 +36,6 @@ const logger = {
 }
 
 logGen.mockReturnValue(logger)
-
 const email = require('./mailer');
 
 describe('Test mailgun', ()=>{
@@ -33,11 +43,13 @@ describe('Test mailgun', ()=>{
         jest.clearAllMocks()
     })
     
-    it('should test if mocksend and mailgun is called', ()=>{
-        email.sendConfirmationEmail('paul@github.com', 'token123')
+    it('should test if mocksend and mailgun is called', async ()=>{
+        await email.sendConfirmationEmail('paul@github.com', 'token123')
         expect(sendFn).toHaveBeenCalledTimes(1)
         expect(sendFn.mock.calls[0][0]).toMatchSnapshot()
-        expect(logger.info.mock.calls[0][0]).toEqual('hi')
+        expect(logger.info.mock.calls[0][0]).toEqual('Confirmation Email successfully sent')        
+        expect(logger.error.mock.calls[0][0]).toEqual("Confirmation Email Error:")
         // expect(logger.info).toHaveBeenCalledTimes(1)
+        
     })
 })
