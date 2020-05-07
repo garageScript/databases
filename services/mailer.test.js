932ea20ce39a9018cb044e8a9b50209f9f6e76f9
unknown <widesun@gmail.com>
2020-05-07 09:57:16 +0900

    #16: fixes mailer test

--- services/mailer.test.js
+++ services/mailer.test.js
@@ -8,12 +8,7 @@ logGen.mockReturnValue(logger)
 
 jest.mock('mailgun-js')
 const mailgun = require('mailgun-js');
-const messages = {
-    send: jest.fn().mockImplementation((data) => {
-        if (data.to) return Promise.resolve('hello')
-        return Promise.reject('rejected')
-    })
-}
+const messages = {}
 mailgun.mockImplementation(() => {
     return {
         messages: () => {
@@ -21,6 +16,7 @@ mailgun.mockImplementation(() => {
         }
     }
 })
+
 const email = require('./mailer');
 
 describe('Test mailgun', ()=>{
@@ -29,6 +25,7 @@ describe('Test mailgun', ()=>{
     })
 
     it('should test if mocksend and mailgun is called', async () => {
+        messages.send = jest.fn().mockReturnValue(Promise.resolve('hello'))
         await email.sendConfirmationEmail('paul@github.com', 'token123')
         expect(messages.send).toHaveBeenCalledTimes(1)
         expect(messages.send.mock.calls[0][0]).toMatchSnapshot()
@@ -37,6 +34,7 @@ describe('Test mailgun', ()=>{
     })
 
     it('should call logger.error when function is called with invalid argument', async () => {
+        messages.send = jest.fn().mockReturnValue(Promise.reject('rejected'))
         await email.sendConfirmationEmail(null, null)
         expect(logger.error).toHaveBeenCalledTimes(1)
         expect(logger.error.mock.calls[0][0]).toEqual('Confirmation Email Error:')
