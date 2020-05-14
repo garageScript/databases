e870bc7f9f908a8dc1b252d45189e6f37c9d549f
unknown <widesun91@gmail.com>
2020-05-14 22:44:10 +0900

    fixes pg.test

--- database/postgres/pg.test.js
+++ database/postgres/pg.test.js
@@ -1,6 +1,7 @@
 jest.mock('pg')
 const {Client} = require('pg')
 const {startPGDB, closePGDB, createPgAccount, deletePgAccount} = require('./pg')
+const logger = {error: jest.fn()}
 
 describe('Test PG DB', ()=>{
   beforeEach(()=>{
@@ -35,22 +36,25 @@ describe('Test PG DB', ()=>{
       it('it should execute all queries if required arguments are passed into createPgAccount', async ()=>{
         await createPgAccount('username', 'password')
         expect(mockClient.query).toHaveBeenCalledTimes(3)
-        expect(mockClient.query).toHaveBeenNthCalledWith(1, `CREATE DATABASE IF NOT EXISTS username`)
-        expect(mockClient.query).toHaveBeenNthCalledWith(2, `CREATE USER IF NOT EXISTS username WITH ENCRYPTED password 'password'`)
-        expect(mockClient.query).toHaveBeenNthCalledWith(3, `GRANT ALL PRIVILEGES ON DATABASE username TO username`)
+        expect(mockClient.query.mock.calls[0][0]).toEqual(`CREATE DATABASE IF NOT EXISTS $1`)
+        expect(mockClient.query.mock.calls[0][1][0]).toEqual('username')
+        expect(mockClient.query.mock.calls[1][0]).toEqual(`CREATE USER IF NOT EXISTS $1 WITH ENCRYPTED password $2`)
+        expect(mockClient.query.mock.calls[1][1][0]).toEqual('username')
+        expect(mockClient.query.mock.calls[1][1][1]).toEqual('password')
+        expect(mockClient.query.mock.calls[2][0]).toEqual(`GRANT ALL PRIVILEGES ON DATABASE $1 TO $1`)
+        expect(mockClient.query.mock.calls[2][1][0]).toEqual('username')
       })
       it('it should not execute any queries in createPgAccount if required arguments are not passed in', async ()=>{
         await createPgAccount()
         expect(mockClient.query).toHaveBeenCalledTimes(0)
       })
-      it('it should check if console.log is called at throw of createPgAccount', async ()=>{
+      it('it should check if logger.error is called at throw of createPgAccount', async ()=>{
         try{
-          console.log = jest.fn() 
           await mockClient.query.mockReturnValue(Promise.reject())
           const resCreatePgAccount = await createPgAccount('username', 'password')
           expect(resCreatePgAccount).rejects.toThrow()
         }catch(err){
-          expect(console.log).toHaveBeenCalledTimes(1)
+          expect(logger.error).toHaveBeenCalledTimes(1)
         }
       })
     })
@@ -59,21 +63,22 @@ describe('Test PG DB', ()=>{
         mockClient.query.mockReturnValue(Promise.resolve())
         await deletePgAccount('username')
         expect(mockClient.query).toHaveBeenCalledTimes(2)
-        expect(mockClient.query).toHaveBeenNthCalledWith(1, `DROP DATABASE IF EXISTS username`)
-        expect(mockClient.query).toHaveBeenNthCalledWith(2, `DROP USER IF EXISTS username`)
+        expect(mockClient.query.mock.calls[0][0]).toEqual(`DROP DATABASE IF EXISTS $1`)
+        expect(mockClient.query.mock.calls[0][1][0]).toEqual('username')
+        expect(mockClient.query.mock.calls[1][0]).toEqual(`DROP USER IF EXISTS $1`)
+        expect(mockClient.query.mock.calls[1][1][0]).toEqual('username')
       })
       it('it should not execute any queries in deletePgAccount if required arguments are not passed in', async ()=>{
         await deletePgAccount()
         expect(mockClient.query).toHaveBeenCalledTimes(0)
       })
-      it('it should check if console.log is called at throw of deletePgAccount', async ()=>{
+      it('it should check if logger.error is called at throw of deletePgAccount', async ()=>{
         try{
-          console.log = jest.fn() 
           await mockClient.query.mockReturnValue(Promise.reject())
           const resDeletePgAccount = await deletePgAccount('username', 'password')
           expect(resDeletePgAccount).rejects.toThrow()
         }catch(err){
-          expect(console.log).toHaveBeenCalledTimes(1)
+          expect(logger.error).toHaveBeenCalledTimes(1)
         }
       })
     })
