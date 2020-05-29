dd7489e64212159f1ee06ab129f1ae6fd8c008e2
greenc <zonxa14@gmail.com>
2020-05-28 17:57:28 -0700

    #61 send reset password

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const db = require('../sequelize/db')
+const {sendPasswordResetEmail} = require('../lib/users')
+
 let server = null
 let app = null
 
@@ -8,7 +11,31 @@ const getApp = () => {
 
 const startServer = (portNumber) => {
   return new Promise((resolve, reject) => {
-   app = express()
+    app = express()
+
+    app.use(express.json()) // for parsing application/json
+    app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+    app.post('/api/notification', (req, res) => {
+      const email = req.body.email
+      const {Accounts} = db.getModels()
+      const userAccount = await Accounts.findOne({
+        where {
+        email: email
+        }
+      })
+      try {
+        sendPasswordResetEmail(userAccount) 
+        res.status(200).send('success')
+      } catch (err) {
+       return {
+         error : {
+           message: 'Email delivery failed. Please try again'
+         }
+        }
+      }
+    })
+
    server =  app.listen(portNumber, () => {
      resolve(app)
     console.log(`Listening on portNumber ${portNumber}`)
