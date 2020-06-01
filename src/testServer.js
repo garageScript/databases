ab52b0ece4b9d76f96ad773e2726aba96d2f3030
unknown <widesun91@gmail.com>
2020-06-01 23:16:19 +0900

    removed temporary files

--- src/testServer.js
+++
@@ -1,9 +0,0 @@
-const {startServer, stopServer, getApp} = require('./server')
-const db = require('../sequelize/db')
-
-const startTest = async () => {
-    await db.start()
-    startServer(3000)
-}
-
-startTest()
