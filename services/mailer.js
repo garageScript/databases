8592d7b3eddad7d0cfcc2780e883f146e1326793
greenc <zonxa14@gmail.com>
2020-06-08 10:06:57 -0700

    #68 testing for valid email and valid account

--- services/mailer.js
+++ services/mailer.js
@@ -2,7 +2,10 @@ const mailgun = require('mailgun-js')
 const logger = require('../lib/log')(__filename)
 require('dotenv').config()
 
-const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN })
+const mg = mailgun({ 
+  apiKey: process.env.MAILGUN_API_KEY, 
+  domain: process.env.MAILGUN_DOMAIN 
+})
 
 const mgModule = {}
 
--- src/routes/userRoutes.test.js
+++ src/routes/userRoutes.test.js
@@ -1,5 +1,46 @@
-jest.mock('express')
+jest.mock('mailgun-js')
+jest.mock('../../sequelize/db')
+jest.mock('../../services/mailer')
 
-describe('Testing the routes', () => {
+const {resetPassword} = require('./userRoutes')
+const {sendPasswordResetEmail} = require('../../lib/users')
+const db = require('../../sequelize/db')
+const email = require('../../services/mailer')
 
+const mockJson = jest.fn()
+
+const res = {
+  status: jest.fn().mockReturnValue({json: mockJson})
+}
+
+describe('Testing user routes', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  test('should throw error if req body does not have email', async () => {
+    const req = {
+      body: {}
+    }
+
+    await resetPassword(req, res)
+    expect(res.status.mock.calls[0][0]).toEqual(400)
+    expect(mockJson.mock.calls[0][0]).toEqual({error: {message: "invalid input"}})
+  })
+  test('should throw error if user account is not found', async () => {
+    db.getModels = jest.fn().mockReturnValue({
+      Accounts: {
+        findOne: jest.fn() 
+      } 
+    }) 
+
+   const req = {
+     body: {
+       email: 'hello@world.com'
+     } 
+   } 
+
+    await resetPassword(req, res)
+    expect(res.status.mock.calls[0][0]).toEqual(400)
+    expect(mockJson.mock.calls[0][0]).toEqual({error: {message: "account does not exist"}})
+  })
 })
