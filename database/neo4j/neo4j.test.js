35c3a70d07983e53fa115d2888602f7d8ed7819b
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-13 00:39:11 -0700

    closes #173 - move mocks to global scope for greater readability

--- database/neo4j/neo4j.test.js
+++ database/neo4j/neo4j.test.js
@@ -1,16 +1,15 @@
 const { startNeo4j, closeNeo4j } = require('./neo4j')
-const neo4j = require('neo4j-driver')
+const neo4j = {}
+
+//needed for startNeo4j
+neo4j.driver = jest.fn().mockReturnValue({})
+
+// driver and driver.close needed for closeNeo4j
+const driver = neo4j.driver()
+driver.close = jest.fn().mockReturnValue(Promise.resolve())
 
 describe('Neo4j database', () => {
-  //needed for startNeo4j
-  neo4j.driver = jest.fn().mockReturnValue({})
-
-  //needed for closeNeo4j
-  const driver = neo4j.driver()
-
   beforeEach(startNeo4j)
-
-  driver.close = jest.fn().mockReturnValue(Promise.resolve())
 
   test('Should resolve when closeNeo4j is called', async () => {
     const res = await closeNeo4j()
