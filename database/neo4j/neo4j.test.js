3f97c3bee81512af9efddb9307e2df5ad09ca091
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-13 00:44:02 -0700

    closes #173 - add more comments for greater clarity

--- database/neo4j/neo4j.test.js
+++ database/neo4j/neo4j.test.js
@@ -9,6 +9,10 @@ const driver = neo4j.driver()
 driver.close = jest.fn().mockReturnValue(Promise.resolve())
 
 describe('Neo4j database', () => {
+  /*
+	startNeo4j initializes the `driver` variable with a value,
+	so it must always be called before running any neo4j related tests.
+	*/
   beforeEach(startNeo4j)
 
   test('Should resolve when closeNeo4j is called', async () => {
