fb78e341211c0120976e5a28d77bda2c0c18fd87
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-08-18 15:30:32 -0700

    closes #173 - make prettierrc file for styling consistency, update files to conform to new rules

--- database/neo4j/neo4j.js
+++ database/neo4j/neo4j.js
@@ -1,18 +1,18 @@
-const neo4j = require('neo4j-driver')
-require('dotenv').config()
+const neo4j = require("neo4j-driver");
+require("dotenv").config();
 
-let driver
-const neo4jModule = {}
+let driver;
+const neo4jModule = {};
 
 neo4jModule.startNeo4j = () => {
   driver = neo4j.driver(
-    'neo4j://104.168.169.204',
+    "neo4j://104.168.169.204",
     neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
-  )
-}
+  );
+};
 
 neo4jModule.closeNeo4j = async () => {
-  await driver.close()
-}
+  await driver.close();
+};
 
-module.exports = neo4jModule
+module.exports = neo4jModule;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const logger = require('../lib/log')(__filename)
-const dbModule = require('../sequelize/db')
-const session = require('express-session')
-const pgModule = require('../database/postgres/pg')
+const express = require("express");
+const logger = require("../lib/log")(__filename);
+const dbModule = require("../sequelize/db");
+const session = require("express-session");
+const pgModule = require("../database/postgres/pg");
 const {
   resetPasswordEmail,
   createUser,
@@ -12,27 +12,27 @@ const {
   userResetPassword,
   updateDBPassword,
   createDatabase,
-} = require('./routes/userRoutes')
-const { postgres } = require('./routes/renderRoutes')
-require('dotenv').config()
-let server = null
-let app = null
-const neo4jModule = require('../database/neo4j/neo4j')
+} = require("./routes/userRoutes");
+const { postgres } = require("./routes/renderRoutes");
+require("dotenv").config();
+let server = null;
+let app = null;
+const neo4jModule = require("../database/neo4j/neo4j");
 
 const getApp = () => {
-  return app
-}
+  return app;
+};
 
 const startServer = async (portNumber) => {
-  await dbModule.start()
-  await pgModule.startPGDB()
-  neo4jModule.startNeo4j()
+  await dbModule.start();
+  await pgModule.startPGDB();
+  neo4jModule.startNeo4j();
 
   return new Promise((resolve, reject) => {
-    app = express()
-    app.set('view engine', 'ejs')
-    app.use(express.json())
-    app.use(express.static('public'))
+    app = express();
+    app.set("view engine", "ejs");
+    app.use(express.json());
+    app.use(express.static("public"));
     app.use(
       session({
         secret: process.env.SECRET,
@@ -44,57 +44,57 @@ const startServer = async (portNumber) => {
           maxAge: 1000 * 60 * 60 * 24 * 7,
         },
       })
-    )
-    app.set('view engine', 'ejs')
-    app.use(express.json())
-    app.get('/', (req, res) => {
-      res.render('welcome', { email: req.session.email })
-    })
-    app.get('/signin', (req, res) => {
-      res.render('signin', { email: req.session.email })
-    })
-    app.get('/setDBpassword', (req, res) => {
-      res.render('setDBpassword', { email: req.session.email })
-    })
-    app.get('/signup', (req, res) => {
-      res.render('signup', { email: req.session.email })
-    })
-    app.get('/setPassword/:token', (req, res) => {
-      res.render('setPassword', { email: req.session.email })
-    })
-    app.get('/resetPassword', (req, res) => {
-      res.render('resetPassword', { email: req.session.email })
-    })
-    app.get('/postgres', postgres)
-    app.post('/api/notifications', resetPasswordEmail)
-    app.post('/api/users', createUser)
-    app.patch('/api/users/:id', updateDBPassword)
-    app.delete('/api/users/:id', deleteUser)
-    app.post('/api/session', loginUser)
-    app.delete('/api/session', logoutUser)
-    app.post('/api/passwordReset', userResetPassword)
-    app.post('/api/createDatabase', createDatabase)
+    );
+    app.set("view engine", "ejs");
+    app.use(express.json());
+    app.get("/", (req, res) => {
+      res.render("welcome", { email: req.session.email });
+    });
+    app.get("/signin", (req, res) => {
+      res.render("signin", { email: req.session.email });
+    });
+    app.get("/setDBpassword", (req, res) => {
+      res.render("setDBpassword", { email: req.session.email });
+    });
+    app.get("/signup", (req, res) => {
+      res.render("signup", { email: req.session.email });
+    });
+    app.get("/setPassword/:token", (req, res) => {
+      res.render("setPassword", { email: req.session.email });
+    });
+    app.get("/resetPassword", (req, res) => {
+      res.render("resetPassword", { email: req.session.email });
+    });
+    app.get("/postgres", postgres);
+    app.post("/api/notifications", resetPasswordEmail);
+    app.post("/api/users", createUser);
+    app.patch("/api/users/:id", updateDBPassword);
+    app.delete("/api/users/:id", deleteUser);
+    app.post("/api/session", loginUser);
+    app.delete("/api/session", logoutUser);
+    app.post("/api/passwordReset", userResetPassword);
+    app.post("/api/createDatabase", createDatabase);
 
     server = app.listen(portNumber, () => {
-      resolve(app)
-      logger.info(`Listening on portNumber ${portNumber}`)
-    })
-  })
-}
+      resolve(app);
+      logger.info(`Listening on portNumber ${portNumber}`);
+    });
+  });
+};
 
 const stopServer = () => {
   return new Promise((resolve, reject) => {
-    dbModule.close()
-    logger.info('DB has been closed')
+    dbModule.close();
+    logger.info("DB has been closed");
     server.close(() => {
-      logger.info('The server has been closed')
-      resolve()
-    })
-  })
-}
+      logger.info("The server has been closed");
+      resolve();
+    });
+  });
+};
 
 module.exports = {
   startServer,
   stopServer,
   getApp,
-}
+};
