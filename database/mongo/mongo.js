69832802fd695529ffd3a42033d9dc079c8dbfdb
Greenc123 <zonxa14@gmail.com>
2020-09-13 23:30:40 +0000

    close #18 created startMongo and closeMongo to connect and disconnect to local Mongo database

--- database/mongo/mongo.js
+++ database/mongo/mongo.js
@@ -4,50 +4,27 @@ const assert = require("assert");
 require("dotenv").config();
 
 const mongoModule = {};
-let mongoClient;
-let db;
+let globalClient;
 
 mongoModule.startMongo = () => {
-  const dbName = "learnDatabases";
-  const url = `mongodb+srv://mongoDB:${process.env.MONGO_PASSWORD}@cluster0.jgkpb.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+  return new Promise((resolve, reject) => {
+    const dbName = "admin";
+    const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@127.0.0.1:27017/${dbName}?retryWrites=true&w=majority`;
 
-  MongoClient.connect(url, (err, client) => {
-    assert.equal(null, err);
-    loggger.info("connected successfully to mongo server");
-
-    db = client.db(dbName);
-    mongoClient = client;
-    return mongoClient;
+    MongoClient.connect(url, (err, client) => {
+      if (err) {
+        return reject(err);
+      }
+      globalClient = client;
+      logger.info("connected successfully to mongo server");
+      return resolve({ status: "success" });
+    });
   });
 };
 
 mongoModule.closeMongo = () => {
-  return mongoClient.close();
-};
-
-mongoModule.createMongo = (username, obj = {}) => {
-  if (!username) return;
-
-  const collection = db.collection(username);
-
-  collection.insert(obj, function (err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    loggger.info(`Inserted ${obj} from ${username} collection`);
-  });
-};
-
-mongoModule.deleteMongo = (username, obj = {}) => {
-  if (!username) return;
-
-  const collection = db.collection(username);
-
-  collection.deleteOne(obj, function (err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    logger.info(`Removed ${obj} from ${username} collection`);
-  });
+  logger.info("disconnecting from mongo server");
+  return globalClient.close();
 };
 
 module.exports = mongoModule;
--- database/mongo/mongo.test.js
+++ database/mongo/mongo.test.js
@@ -1,53 +1,41 @@
 jest.mock("../../lib/log");
 jest.mock("assert");
 jest.mock("mongodb");
+const mongodb = require("mongodb");
 
 const logGen = require("../../lib/log");
-const logger = { error: jest.fn() };
+const logger = { info: jest.fn(), error: jest.fn() };
 logGen.mockReturnValue(logger);
 
-const MongoClient = require("mongodb").MongoClient;
-
-const { startMongo, closeMongo, createMongo, deleteMongo } = require("./mongo");
+const { startMongo, closeMongo } = require("./mongo");
 
 describe("Test MongoDB DB", () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    mongodb.MongoClient.connect = (url, fn) => {
+      fn(null, {});
+    };
   });
 
-  let mockClient = {
-    connect: jest.fn().mockReturnValue(Promise.resolve()),
-    end: jest.fn().mockReturnValue(Promise.resolve()),
-    collection: jest.fn().mockReturnValue(Promise.resolve()),
-  };
-
-  describe("Test startMongoDB and closeMongoDB", () => {
-    it("it should connect to MongoDB when startMongo is called", async () => {
-      await startMongo;
-      expect(mockClient.connect).toHaveBeenCalledTimes(1);
-    });
-    it("should call end when closing MongoDB", async () => {
-      await closeMongo;
-      expect(mockClient.end).toHaveBeenCalledTimes(1);
-    });
+  it("it should connect to MongoDB when startMongo is called", async () => {
+    const data = await startMongo();
+    expect(data).toEqual({ status: "success" });
   });
-  describe("Test create and delete MongoDB collection", () => {
-    beforeEach(async () => {
-      await startMongo;
-    });
-    afterEach(async () => {
-      await closeMongo;
-    });
-    describe("Create MongoDB collection", () => {
-      it("should create a collection for user", async () => {
-        await createMongo("Jeff Bezos", { a: 1, b: 2, c: 3 });
-        expect(mockClient.collection).toHaveBeenCalledTimes(1);
-      });
-      it("should delete a collection for a user", async () => {
-        await createMongo("Jeff Bezos", { a: 1, b: 2, c: 3 });
-        await deleteMongo("Jeff Bezos", { a: 1, b: 2, c: 3 });
-        expect(mockClient.collection).toHaveBeenCalledTimes(2);
-      });
-    });
+  it("it should reject if there is an error", async () => {
+    mongodb.MongoClient.connect = (url, fn) => {
+      fn(new Error("cannot connect"));
+    };
+    return expect(startMongo()).rejects.toThrowError("cannot connect");
+  });
+  it("it should call client close", async () => {
+    const client = {
+      close: jest.fn(),
+    };
+    mongodb.MongoClient.connect = (url, fn) => {
+      fn(null, client);
+    };
+    await startMongo();
+    await closeMongo();
+    return expect(client.close).toHaveBeenCalledTimes(1);
   });
 });
