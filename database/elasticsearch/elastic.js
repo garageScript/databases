fe193b83bff44678e76b2d9cb5d05c6958ca1c85
anthonykhoa <45890848+anthonykhoa@users.noreply.github.com>
2020-10-05 08:39:10 -0700

    Update elastic.js
    merge vonflict resolutoin

--- database/elasticsearch/elastic.js
+++ database/elasticsearch/elastic.js
@@ -79,7 +79,7 @@ es.deleteAccount = async (account) => {
     null,
     authorization
   );
-  const r3 = await sendESRequest(`/${account.username}-*`, "DELETE");
+  const r3 = await sendFetch(`/${account.username}-*`, "DELETE", null, authorization);
   const err = r1.error || r2.error;
   if (err || !r1.found || !r2.found) {
     logger.error("Deleting Elasticsearch user failed");
